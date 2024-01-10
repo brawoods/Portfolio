1b650572811adbb45b01d62d48f37e6fb56997b0
Brandon Woods <woods.brandon.m@gmail.com>
2024-01-09 18:34:36 -0800

    create placehoplaceholder project data

---
+++ src/app/lib/placeholder-project-data.ts
@@ -0,0 +1,25 @@
+const placeholderProject = {
+  name: "black clover",
+  descriptions: {
+    short: "e-commerce product page",
+    full: "An e-commerce product page. Features include product overview, related items, saving items to user account, user questions and answers, user product reviews",
+  },
+  gitHubUrl: "https://github.com/Black-Clover-FEC/black-clover-FEC",
+  technologies: [
+    "JAVASCRIPT",
+    "REACT",
+    "REACT ROUTER",
+    "EXPRESS.JS",
+    "STYLED COMPONENTS",
+    "HTML5",
+    "CSS3",
+  ],
+  images: ["../../public/assets/black-clover/black-clover-product-page.png"],
+  gifs: [
+    "../../public/assets/black-clover/0productIndex.gif",
+    "../../public/assets/black-clover/1productOverview.gif",
+    "../../public/assets/black-clover/2relatedProducts.gif",
+    "../../public/assets/black-clover/3reviewsAddVoteReport.gif",
+    "../../public/assets/black-clover/3reviewsFilterAndSort.gif",
+  ],
+};
