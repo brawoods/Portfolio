20009799c2e4a6516e55a25e02b86c11f41a94fc
Brandon Woods <woods.brandon.m@gmail.com>
2024-02-12 15:29:34 -0800

    move types to serparate file

--- src/app/lib/project-data.ts
+++ src/app/lib/project-data.ts
@@ -1,8 +1,6 @@
-import productPage from "@/../public/assets/black-clover/black-clover-product-page.png";
-
 const blackClover = {
   name: "Black Clover",
-  descriptions:
+  description:
     "An e-commerce product page. Features include product overview, related items, saving items to user account, user questions and answers, user product reviews",
   gitHubUrl: "https://github.com/Black-Clover-FEC/black-clover-FEC",
   technologies: [
@@ -14,19 +12,19 @@ const blackClover = {
     "HTML5",
     "CSS3",
   ],
-  images: [productPage],
+  images: ["/assets/black-clover/black-clover-product-page.png"],
   gifs: [
-    "../../public/assets/black-clover/0productIndex.gif",
-    "../../public/assets/black-clover/1productOverview.gif",
-    "../../public/assets/black-clover/2relatedProducts.gif",
-    "../../public/assets/black-clover/3reviewsAddVoteReport.gif",
-    "../../public/assets/black-clover/3reviewsFilterAndSort.gif",
+    "./public/assets/black-clover/0productIndex.gif",
+    "./public/assets/black-clover/1productOverview.gif",
+    "./public/assets/black-clover/2relatedProducts.gif",
+    "./public/assets/black-clover/3reviewsAddVoteReport.gif",
+    "./public/assets/black-clover/3reviewsFilterAndSort.gif",
   ],
 };
 
 const stackage = {
   name: "Stackage",
-  descriptions:
+  description:
     "StackageApp is a community-powered, self-policing fully mobile social media platform for sharing information about technology. In the application, authenticated users can make and share posts, view and filter a feed of others' posts, approve posts for inclusion in the feed, make a profile, view and connect with other users, message connections, and more. Limited features are also available to unauthenticated users when they sign in as a guest. StackageApp is an open source project.",
   gitHubUrl: "https://github.com/StackageApp/stackage-app",
   technologies: [
@@ -38,13 +36,13 @@ const stackage = {
     "JEST",
     "FIREBASE",
   ],
-  images: [],
+  images: ["/assets/black-clover/black-clover-product-page.png"],
   gifs: [],
 };
 
 const bluegillApi = {
   name: "Bluegill API",
-  descriptions:
+  description:
     'BluegillAPI is a RESTful API that allows for the fast and secure access of data stored for an e-commerce application. It was created to replace a legacy API service. The API supports endpoints for the "Questions and Answers" segment of the product page.',
   gitHubUrl: "",
   technologies: [
@@ -55,14 +53,10 @@ const bluegillApi = {
     "NODE.JS",
     "UBUNTU",
   ],
-  images: [],
+  images: ["/assets/black-clover/black-clover-product-page.png"],
   gifs: [],
 };
 
-const projects = {
-  blackClover: blackClover,
-  stackage: stackage,
-  bluegillApi: bluegillApi,
-};
+const projects = [blackClover, stackage, bluegillApi];
 
 export { projects };
---
+++ src/app/lib/types.ts
@@ -0,0 +1,8 @@
+interface Project {
+  name: string;
+  description: string;
+  gitHubUrl: string;
+  technologies: string[];
+  images: string[];
+  gifs: string[];
+}
